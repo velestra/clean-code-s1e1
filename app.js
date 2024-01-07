eafbb1cddb3fb95532775038a911ae255d2113a7
velestra <150198911+velestra@users.noreply.github.com>
2024-01-07 15:37:19 -0600

    fix(basic-3.4): tag selectors

--- app.js
+++ app.js
@@ -41,9 +41,9 @@ var createNewTaskElement=function(taskString){
   editInput.className="task";
 
   editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-  editButton.className="edit";
+  editButton.className="button edit";
 
-  deleteButton.className="delete";
+  deleteButton.className="button delete";
   deleteButtonImg.src='./remove.svg';
   deleteButton.appendChild(deleteButtonImg);
 
